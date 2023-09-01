bcf767fa153ec3b5d29c1d6f6bf69be53ac48a53
0xLaurens <lauwiv@gmail.com>
2023-09-01 16:25:11 +0200

    Trace (back) -> tracing added

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,8 +1,14 @@
 mod app;
 mod ws;
+mod services;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app::create_app())
         .await
