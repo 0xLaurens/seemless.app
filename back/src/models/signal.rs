3c1b2454cd053855871196dc188b8a054c894f52
0xLaurens <lauwiv@gmail.com>
2023-09-04 15:47:14 +0200

    models (backend) -> signal types

---
+++ back/src/models/signal.rs
@@ -0,0 +1,16 @@
+
+/*
+* different states of the application
+* messages should be annotated with the type of signal
+*/
+
+pub enum SignalType {
+    Offer,
+    Answer,
+    NewIceCandidate,
+    UserIdentity,
+    PeerJoined,
+    PeerLeft,
+    Peers,
+}
+
