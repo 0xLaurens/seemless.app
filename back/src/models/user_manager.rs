fc90d607f1371bed189e5af5de8c501b68786412
0xLaurens <lauwiv@gmail.com>
2023-09-05 11:05:56 +0200

    models (back) -> user management comment

--- back/src/models/user_manager.rs
+++ back/src/models/user_manager.rs
@@ -1,5 +1,10 @@
 use crate::models::user::{User, Username};
 
+
+/*
+* User management interface to easily change impl
+* it's purpose is to manage the users connected and available to connect to.
+*/
 trait UserManager {
     fn add_user(&self, user: User) -> User;
     fn remove_user(&self, username: Username) -> User;
