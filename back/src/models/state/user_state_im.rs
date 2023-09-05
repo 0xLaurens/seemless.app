67c058ec7ecb5b3d1d9d73695ef5a2248d27c033
0xLaurens <lauwiv@gmail.com>
2023-09-05 13:14:52 +0200

    models (back) -> user manager impl for in memory user state

--- back/src/models/state/user_state_im.rs
+++ back/src/models/state/user_state_im.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use tracing::Instrument;
 use crate::models::user::{User, Username};
+use crate::models::user_manager::UserManager;
 
 struct UserStateInMemory {
     users: Arc<Mutex<HashMap<Username, User>>>
@@ -11,5 +13,35 @@ impl UserStateInMemory {
         Self {
             users: Arc::new(Mutex::new(HashMap::new()))
         }
+    }
+}
+
+impl UserManager for UserStateInMemory {
+    fn add_user(&self, user: User) -> Option<User> {
+        self.users.lock()
+            .insert(user.get_username(), user.clone())
+    }
+
+    fn remove_user(&self, username: Username) -> User {
+        self.users
+            .lock()
+            .remove(username)?
+    }
+
+    fn update_user(&self, user: User, username: Username) -> Option<User> {
+       let _ = self.users
+            .lock()?
+            .remove(&username);
+
+        self.users
+            .lock()?
+            .insert(username, user)
+    }
+
+    fn get_users(&self) -> Vec<User> {
+        self.users
+            .clone()
+            .values()
+            .collect()
     }
 }
--- back/src/models/user_manager.rs
+++ back/src/models/user_manager.rs
@@ -5,9 +5,9 @@ use crate::models::user::{User, Username};
 * User management interface to easily change impl
 * it's purpose is to manage the users connected and available to connect to.
 */
-trait UserManager {
-    fn add_user(&self, user: User) -> User;
-    fn remove_user(&self, username: Username) -> User;
-    fn update_user(&self, user: User, username: Username) -> User;
+pub trait UserManager {
+    fn add_user(&self, user: User) -> Option<User>;
+    fn remove_user(&self, username: Username) -> Option<User>;
+    fn update_user(&self, user: User, username: Username) -> Option<User>;
     fn get_users(&self) -> Vec<User>;
 }
