d5df01e159c34cacd23fe394b2f39bff66213191
0xLaurens <lauwiv@gmail.com>
2023-09-14 16:51:07 +0200

    ws (BE) -> request matcher based on type

--- backend/handlers/ws.go
+++ backend/handlers/ws.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"laurensdrop/data"
 	"log"
 )
 
@@ -21,10 +23,52 @@ func WSHandler(c *websocket.Conn) {
 			break
 		}
 		log.Printf("recv: %s", msg)
+		RequestMatcher(msg)
 
 		if err = c.WriteMessage(mt, msg); err != nil {
 			log.Println("write:", err)
 			break
 		}
 	}
+}
+
+func ParseMessageToRequest(msg []byte) (data.Request, error) {
+	req := data.Request{}
+	err := json.Unmarshal(msg, &req)
+	if err != nil {
+		return data.Request{}, err
+	}
+
+	return req, nil
+}
+
+func RequestMatcher(msg []byte) {
+	req, err := ParseMessageToRequest(msg)
+	if err != nil {
+		log.Println("NotJSON")
+		return
+	}
+
+	switch req.Type {
+	case data.Offer:
+		log.Println("Offer")
+		break
+	case data.Answer:
+		log.Println("Answer")
+		break
+	case data.PeerLeft:
+		log.Println("PeerLeft")
+		break
+	case data.PeerJoined:
+		log.Println("PeerJoined")
+		break
+	case data.PeerUpdated:
+		log.Println("PeerUpdated")
+		break
+	case data.NewIceCandidate:
+		log.Println("NewIceCandidate")
+	default:
+		log.Println("InvalidRequest")
+	}
+
 }
