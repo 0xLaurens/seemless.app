92571206bdfad98f4e2ca230e6ad93e85931c41d
0xLaurens <lauwiv@gmail.com>
2023-09-15 14:33:43 +0200

    user (BE) -> added ws conn into struct

--- backend/data/user.go
+++ backend/data/user.go
@@ -1,20 +1,26 @@
 package data
 
-import "github.com/google/uuid"
+import (
+	"github.com/gofiber/contrib/websocket"
+	"github.com/google/uuid"
+)
 
 type UserID string
+type Conn *websocket.Conn
 
 type User struct {
-	ID       UserID `json:"client_id"`
-	Username string `json:"username"`
-	Device   string `json:"device"`
+	ID         UserID `json:"client_id"`
+	Username   string `json:"username"`
+	Device     string `json:"device"`
+	Connection Conn   `json:"connection"`
 }
 
-func CreateUser(Username string, Device string) *User {
+func CreateUser(Username string, Device string, conn Conn) *User {
 	return &User{
-		ID:       UserID(uuid.NewString()),
-		Username: Username,
-		Device:   Device,
+		ID:         UserID(uuid.NewString()),
+		Username:   Username,
+		Device:     Device,
+		Connection: conn,
 	}
 }
 
@@ -28,4 +34,8 @@ func (u *User) GetDevice() string {
 
 func (u *User) UpdateUsername(Username string) {
 	u.Username = Username
+}
+
+func (u *User) GetConnection() Conn {
+	return u.Connection
 }
