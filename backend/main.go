01c6b2b53df52d74c5792494c4b5d7503d5e743b
0xLaurens <lauwiv@gmail.com>
2023-09-18 09:29:45 +0200

    main (BE) -> create store and hub

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"laurensdrop/handlers"
+	"laurensdrop/store"
 	"log"
 )
 
 func main() {
+	// init in memory store and hub
+	s := store.NewUserStoreInMemory()
+	hub := handlers.CreateHub(s)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -15,7 +20,9 @@ func main() {
 	})
 
 	app.Use("/ws", handlers.WSUpgrader)
-	app.Use("/ws", websocket.New(handlers.WSHandler))
+	app.Use("/ws", websocket.New(func(conn *websocket.Conn) {
+		handlers.WSHandler(conn, hub)
+	}))
 
 	log.Fatal(app.Listen(":3000"))
 }
