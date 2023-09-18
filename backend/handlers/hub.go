463b238a5afa5060b1c15d85480cce8b1036d2fe
0xLaurens <lauwiv@gmail.com>
2023-09-18 11:23:14 +0200

    hub (BE) -> invalid message channel

--- backend/handlers/hub.go
+++ backend/handlers/hub.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
 	"laurensdrop/data"
 	"laurensdrop/store"
 	"log"
@@ -8,16 +10,18 @@ import (
 
 // Channels are for the different actions that are listened for
 type Channels struct {
-	broadcast  chan []byte
-	register   chan *data.User
-	unregister chan *data.User
+	broadcast      chan []byte
+	register       chan *data.User
+	unregister     chan *data.User
+	invalidMessage chan *data.User
 }
 
 func createChannels() Channels {
 	return Channels{
-		broadcast:  make(chan []byte),
-		register:   make(chan *data.User),
-		unregister: make(chan *data.User),
+		broadcast:      make(chan []byte),
+		register:       make(chan *data.User),
+		unregister:     make(chan *data.User),
+		invalidMessage: make(chan *data.User),
 	}
 }
 
@@ -64,6 +68,21 @@ func (h *Hub) Run() {
 				log.Fatal("ERR -->> unregister failed", err)
 				return
 			}
+		case user := <-h.channels.invalidMessage:
+			log.Println("DBG -->> invalid message")
+			errRes := fiber.NewError(int(data.WsError.InvalidRequestBody))
+			res, err := json.Marshal(errRes)
+			if err != nil {
+				log.Println("ERR -->> failed to create json from err", err)
+				return
+			}
+
+			err = user.Connection.Conn.WriteMessage(1, res)
+			if err != nil {
+				log.Println("ERR -->> failed to send message", err)
+				return
+			}
+
 		}
 	}
 }
