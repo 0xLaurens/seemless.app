d6c7bfdb3f0544a26ac0c8a973b92f5430b728ca
0xLaurens <lauwiv@gmail.com>
2023-09-19 14:05:25 +0200

    test (BE) -> user leave test

--- backend/handlers/ws_test.go
+++ backend/handlers/ws_test.go
@@ -336,6 +336,84 @@ func TestWsHandlerUsernameShouldJoin(t *testing.T) {
 	}
 }
 
+func TestWsHandlerUserLeaveMessage(t *testing.T) {
+	app, done := runTestApp()
+	defer app.Shutdown()
+	<-done
+
+	url := "ws://localhost:5420/ws"
+	conn, resp, err := ws.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	conn2, _, err := ws.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return
+	}
+	defer conn2.Close()
+
+	assert.Equal(t, 100, resp.StatusCode)
+	assert.Equal(t, "websocket", resp.Header.Get("Upgrade"))
+
+	expected := fiber.Map{
+		"message": "provide a username",
+		"type":    "UsernamePrompt",
+	}
+	var res fiber.Map
+	err = conn.ReadJSON(&res)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, res)
+
+	req := fiber.Map{
+		"type": "Username",
+		"body": fiber.Map{
+			"username": "user",
+		},
+	}
+
+	err = conn.WriteJSON(req)
+	assert.NoError(t, err)
+
+	err = conn.ReadJSON(&res)
+	assert.NoError(t, err)
+
+	expected = fiber.Map{
+		"type":     data.RequestTypes.PeerJoined,
+		"username": "user",
+	}
+	assert.Equal(t, expected, res)
+
+	req = fiber.Map{
+		"type": "Username",
+		"body": fiber.Map{
+			"username": "user2",
+		},
+	}
+
+	err = conn2.WriteJSON(req)
+	assert.NoError(t, err)
+
+	expected = fiber.Map{
+		"type":     data.RequestTypes.PeerLeft,
+		"username": "user",
+	}
+
+	err = conn2.ReadJSON(res)
+	if err != nil {
+		return
+	}
+
+	conn.Close()
+	assert.Equal(t, expected, res)
+
+	err = app.Shutdown()
+	if err != nil {
+		t.Fatal("TEST ERR -->> failed to shutdown server", err)
+	}
+}
+
 func TestWsHandlerUserShouldReturnDuplicateUsernameError(t *testing.T) {
 	app, done := runTestApp()
 	defer app.Shutdown()
