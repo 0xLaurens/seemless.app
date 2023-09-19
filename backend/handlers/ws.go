0767cd8ca74136aa2fc856223e551215ba5ce80a
0xLaurens <lauwiv@gmail.com>
2023-09-19 10:44:49 +0200

    helpers (BE) -> handler helper func

--- backend/handlers/ws.go
+++ backend/handlers/ws.go
@@ -19,18 +19,20 @@ func WSHandler(c *websocket.Conn, hub *Hub) {
 	username := ""
 	err := c.WriteJSON(fiber.Map{
 		"type":    "UsernamePrompt",
-		"message": "What Username would you like to identify with?",
+		"message": "provide a username",
 	})
+	if err != nil {
+		log.Println("ERR -->> write JSON error")
+		return
+	}
+
 	for username == "" {
-		if err != nil {
-			log.Println("ERR -->> write JSON error")
-			return
-		}
 		_, msg, err := c.ReadMessage()
 		if err != nil {
 			log.Println("ERR -->> read message", err)
 			return
 		}
+
 		req := data.Request{}
 		err = utils.MapJsonToStruct(msg, &req)
 		if err != nil {
@@ -40,49 +42,30 @@ func WSHandler(c *websocket.Conn, hub *Hub) {
 
 		username = req.Body["username"]
 		if username == "" || req.Type != data.RequestTypes.Username {
-			err := c.WriteJSON(fiber.Map{
+			writeMSG(c, fiber.Map{
 				"type":    data.WsError.InvalidRequestBody,
 				"message": "Please Send the right format for the username request",
 			})
-
-			if err != nil {
-				log.Println("ERR -->> couldn't message", err)
-			}
 		}
 
 		if u, _ := hub.users.GetUserByName(username); u != nil {
-			err := c.WriteJSON(fiber.Map{
+			writeMSG(c, fiber.Map{
 				"type":    data.UserStoreError.DuplicateUsername,
 				"message": "duplicate username error",
 			})
-
-			if err != nil {
-				log.Println("ERR -->> couldn't message", err)
-			}
 			username = ""
 		}
 	}
 
-	err = c.WriteJSON(fiber.Map{
-		"code":     200,
+	writeMSG(c, fiber.Map{
 		"type":     data.RequestTypes.PeerJoined,
 		"username": username,
 	})
-	if err != nil {
-		return
-	}
-
 	user := data.CreateUser(username, "android", data.WithConnection(c))
-	defer func() {
-		log.Println("DBG -->> defer ws handler")
-		hub.channels.unregister <- user
-		err := c.Close()
-		if err != nil {
-			log.Println("ERR -->> failed to close connection")
-			return
-		}
-	}()
 	hub.channels.register <- user
+
+	defer wsDefer(hub, user, c)
+
 	log.Printf("DBG -->> created user: %v", user.Username)
 	for {
 		_, msg, err := c.ReadMessage()
@@ -92,6 +75,24 @@ func WSHandler(c *websocket.Conn, hub *Hub) {
 		}
 		log.Printf("DBG -->> recv: %s", msg)
 		WsRequestHandler(msg, hub, user)
+	}
+}
+
+func wsDefer(hub *Hub, user *data.User, c *websocket.Conn) {
+	log.Println("DBG -->> defer ws handler")
+	hub.channels.unregister <- user
+	err := c.Close()
+	if err != nil {
+		log.Println("ERR -->> failed to close connection")
+		return
+	}
+}
+
+func writeMSG(c *websocket.Conn, json interface{}) {
+	err := c.WriteJSON(json)
+	if err != nil {
+		log.Println("ERR -->> failed to writeMSG", err)
+		return
 	}
 }
 
