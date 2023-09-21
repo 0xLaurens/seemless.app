7768707b71ca4a89dc672c6bc390881b84d92460
0xLaurens <lauwiv@gmail.com>
2023-09-21 15:32:00 +0200

    routing (FE) -> routing for username page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UsernameView from "../views/UsernameView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: "/nick",
+      name: "username",
+      component: UsernameView,
+    }
   ]
 })
 
