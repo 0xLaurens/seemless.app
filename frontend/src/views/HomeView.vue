498124ba45f5578cf289590df6d258797eda8bbc
0xLaurens <lauwiv@gmail.com>
2023-09-21 09:32:40 +0200

    home (FE) -> hero section

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,24 +3,16 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-12 gap-12 mx-28 my-48 content-center">
-    <div class="col-span-12 md:col-span-6">
-      <div class="flex flex-col justify-center items-center md:items-start h-full">
-        <h1 class="text-4xl md:text-5xl lg:text-6xl font-black text-white capitalize mb-5">Share files directly from anywhere to anywhere</h1>
-        <p class="text-lg md:text-xl font-medium mb-3">Send files of any size directly to another device without ever storing it online.</p>
-        <a href="#" class="btn btn-primary btn-lg btn-wide">
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img :src="imageSrc" class="rounded-lg shadow-2xl"  alt="tin can telephone"/>
+      <div>
+        <h1 class="max-w-2xl text-4xl md:text-5xl lg:text-6xl font-black text-white capitalize mb-5">Share files directly from anywhere to anywhere</h1>
+        <p class="max-w-xl text-lg md:text-xl font-medium mb-12">Send files of any size directly to another device without ever storing it online.</p>
+        <a href="#" class="btn btn-primary btn-md btn-wide">
           Share Now
         </a>
       </div>
     </div>
-    <div class="col-span-12 md:col-span-6">
-      <img :src="imageSrc" alt="Hero Image" class="w-full h-auto max-w-2xl rounded-2xl">
-    </div>
-    <div class="col-span-12">
-      <div>Blazingly Fast</div>
-      <div>No File Size Limit</div>
-      <div></div>
-      <div></div>
-    </div>
   </div>
-</template>
+  </template>
