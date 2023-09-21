9178d580ebf99ef758ad7c1f6b0a09bf01e87886
0xLaurens <lauwiv@gmail.com>
2023-09-21 10:36:37 +0200

    store (FE) -> user store

---
+++ frontend/src/stores/user.ts
@@ -0,0 +1,19 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+
+export const useUserStore = defineStore('user', () => {
+    const username = ref()
+    function getUsername(): string {
+        return username
+    }
+
+    function setUsername(name: string) {
+        username.value = name
+    }
+
+    return {
+        username,
+        getUsername,
+        setUsername,
+    }
+})
