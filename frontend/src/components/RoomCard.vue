dc182a2ae2a8439ee88891d1f14514b5ea49a2a2
0xLaurens <lauwiv@gmail.com>
2023-09-28 09:05:15 +0200

    room-card (FE) -> hover effect

--- frontend/src/components/RoomCard.vue
+++ frontend/src/components/RoomCard.vue
@@ -12,7 +12,9 @@ defineProps({
 </script>
 
 <template>
-  <div class="card bg-base-300 w-96 shadow-2xl py-6 ml-0 m-12">
+  <div
+    class="card bg-base-300 w-96 shadow-2xl ml-0 m-12 py-6 hover:shadow-primary hover:cursor-pointer"
+  >
     <button
       class="absolute btn btn-primary btn-circle btn-sm self-end mr-3"
       @click="modal.trigger({ title: modalTitle, message: modalMessage, btnText: modalBtnText })"
@@ -21,7 +23,10 @@ defineProps({
     </button>
     <div class="card-body flex items-center text-center">
       <h2 class="card-title text-white text-2xl font-extrabold">{{ title || 'Title' }}</h2>
-      <p class="hind text-base normal-case text-gray-500">{{ description || 'Description' }}</p>
+      <p class="hind text-base normal-case text-gray-500 mb-4">
+        {{ description || 'Description' }}
+      </p>
+      <button class="btn w-full btn-primary">Connect</button>
     </div>
   </div>
 </template>
