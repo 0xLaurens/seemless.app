918fbc96f944af1d84e18304cc31cd61bfadb75c
0xLaurens <lauwiv@gmail.com>
2023-09-29 09:30:39 +0200

    404 (FE) -> 404 page when route not found

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-9xl font-black text-white">404</h1>
+        <h2 class="text-4xl font-bold">Page Not Found</h2>
+        <p class="py-6 hind font-normal text-xl">
+          The page that you were looking for does not exist. It might have been removed or has had
+          its name removed
+        </p>
+        <a href="/" class="btn btn-primary">Go To Homepage</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
