3fea6cde382499c9a0e95b0555425b6c477da419
0xLaurens <lauwiv@gmail.com>
2023-10-06 16:23:34 +0200

    conn (FE) -> keep track of users you are connected with

---
+++ frontend/src/stores/connected.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+// Store for keeping track of the users you are connected to
+export const useConnectedStore = defineStore('connected', () => {
+  const connected = ref(new Map<string, boolean>())
+
+  function createUserConnection(username: string, status: boolean) {
+    connected.value.set(username, status)
+  }
+
+  function getUserConnectionStatus(username: string): boolean | undefined {
+    return connected.value.get(username)
+  }
+
+  function removeUser(username: string) {
+    connected.value.delete(username)
+  }
+
+  function getConnectedUsers() {
+    return connected.value.keys()
+  }
+
+  return {
+    createUserConnection,
+    getUserConnectionStatus,
+    removeUser,
+    getConnectedUsers
+  }
+})
