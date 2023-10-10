dc5e2d1f5ae717f910265feedf6505313ee0622d
0xLaurens <lauwiv@gmail.com>
2023-10-10 14:04:06 +0200

    backoff (fe) -> exponential backoff function

---
+++ frontend/src/stores/retry.ts
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+export const useRetryStore = defineStore('retry', () => {
+  const DELAY: number = 1000
+  const MAX_TIMEOUT: number = 8000 //MAX MS between reload
+
+  const attempts = ref(0)
+  const timer: Ref<number | undefined> = ref()
+
+  function start(fn: () => void) {
+    let backOffDuration = MAX_TIMEOUT
+    if (attempts.value < 3) {
+      backOffDuration = Math.pow(2, attempts.value) * DELAY
+    }
+    console.log('BACKOFF', backOffDuration, 'MS')
+
+    timer.value = setTimeout(() => {
+      fn()
+      attempts.value++
+      start(fn)
+    }, backOffDuration)
+  }
+
+  function stop() {
+    if (timer.value) {
+      clearTimeout(timer.value)
+      timer.value = undefined
+    }
+    attempts.value = 0
+  }
+
+  return { start, stop }
+})
