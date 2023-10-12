2bef3e5bee6ee6cab139bd2d3c25a2321d557a98
0xLaurens <lauwiv@gmail.com>
2023-10-12 14:52:08 +0200

    download (FE) -> download popup

--- frontend/src/components/ConfirmDownload.vue
+++ frontend/src/components/ConfirmDownload.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="h-full w-full">
-    <div class="card w-96 bg-neutral text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">User would like to send you</h2>
-        <p>filename.png</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Accept</button>
-          <button class="btn btn-ghost">Deny</button>
+  <div
+    @click="download.close()"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-400/25 backdrop-filter backdrop-blur"
+    v-if="download.popup"
+    :aria-hidden="download.popup"
+  >
+    <div class="">
+      <div class="card w-96 bg-neutral text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title text-white font-black text-2xl pb-3">
+            Username would like to send you
+          </h2>
+          <p class="text-lg pb-6">filename.png</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-outline">Deny</button>
+            <button class="btn btn-primary">Accept</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDownloadStore } from '@/stores/download'
+
+const download = useDownloadStore()
+</script>
---
+++ frontend/src/models/download.ts
@@ -0,0 +1,4 @@
+export interface Download {
+  file: File
+  from: string
+}
