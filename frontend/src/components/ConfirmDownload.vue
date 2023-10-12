355eab62df1a2e5a3482ca6d4c75f94d65fd0744
0xLaurens <lauwiv@gmail.com>
2023-10-12 10:58:41 +0200

    fix (FE) -> resolve lost files

---
+++ frontend/src/components/ConfirmDownload.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-full w-full">
+    <div class="card w-96 bg-neutral text-neutral-content">
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">User would like to send you</h2>
+        <p>filename.png</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Accept</button>
+          <button class="btn btn-ghost">Deny</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
