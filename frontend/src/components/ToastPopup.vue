fd6648082c147558fdfc346270ee702690165d8d
0xLaurens <lauwiv@gmail.com>
2023-10-13 15:58:57 +0200

    toast (FE) -> toast pill

---
+++ frontend/src/components/ToastPopup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="toast.type.toString()" class="alert">
+    <toast-icon :type="toast.type" />
+    <span>{{ toast.message }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Toast } from '@/models/toast'
+import ToastIcon from '@/components/icons/ToastIcon.vue'
+
+defineProps<{
+  toast: Toast
+}>()
+</script>
