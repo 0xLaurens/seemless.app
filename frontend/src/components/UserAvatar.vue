cf98f5de479e057b307b33e2542169d85c8e3194
0xLaurens <lauwiv@gmail.com>
2023-10-17 16:02:05 +0200

    ring (FE) -> add ring when dc is open

--- frontend/src/components/UserAvatar.vue
+++ frontend/src/components/UserAvatar.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
-import { useConnectedStore } from '@/stores/connected'
 import type { User } from '@/models/user'
 import { useConnStore } from '@/stores/connection'
 import { useWebsocketStore } from '@/stores/websocket'
+import { ref, watch } from 'vue'
+import type { Connection } from '@/models/connection'
 
-const connStore = useConnectedStore()
 const props = defineProps<{ user: User }>()
+const connected = ref(false)
 
 const conn = useConnStore()
 const ws = useWebsocketStore()
@@ -16,13 +17,24 @@ async function sendOffer(username: string) {
   ws.SendMessage(offer)
 }
 
+watch(conn.conn, (newConn) => {
+  const connection: Connection | undefined = newConn.get(props.user.username)
+  if (connection === undefined) return
+  connection.dc?.addEventListener('open', () => {
+    connected.value = true
+  })
+  connection.dc?.addEventListener('close', () => {
+    connected.value = false
+  })
+})
+
 const userStore = useUserStore()
 const isUser = userStore.getUsername() === props.user.username
 </script>
 
 <template>
   <div
-    class="user justify-center text-center mr-6 group"
+    class="user justify-center text-center mr-6 group w-36"
     :class="{ 'cursor-pointer': !isUser, 'cursor-default': isUser }"
     @click="!isUser ? sendOffer(user.username) : null"
   >
@@ -30,14 +42,14 @@ const isUser = userStore.getUsername() === props.user.username
       <div
         :class="{
           'group-hover:ring-4 ring-accent ring-offset-4 ring-offset-base-100': !isUser,
-          'ring-4 ring-accent': connStore.getUserConnectionStatus(props.user.username)
+          'ring-4 ring-accent': connected
         }"
         class="bg-neutral-focus text-neutral-content rounded-full w-24"
       >
         <span class="text-3xl">{{ user.username.charAt(0) }}</span>
       </div>
     </div>
-    <h2>{{ user.username }} <span v-if="isUser">(YOU)</span></h2>
+    <h2 class="break-words">{{ user.username }} <span v-if="isUser">(YOU)</span></h2>
     <span class="hind text-accent text-lg font-medium">{{ user.device }}</span>
   </div>
 </template>
