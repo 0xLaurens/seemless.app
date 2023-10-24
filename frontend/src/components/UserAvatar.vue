802b2dc805a3c93b8915de2f6dcdaf14f8c39eb9
0xlaurens <lauwiv@gmail.com>
2023-10-24 11:07:10 +0200

    sizing (FE) -> mobile

--- frontend/src/components/UserAvatar.vue
+++ frontend/src/components/UserAvatar.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user'
-import type { User } from '@/models/user'
-import { useConnStore } from '@/stores/connection'
-import { useWebsocketStore } from '@/stores/websocket'
+import {useUserStore} from '@/stores/user'
+import type {User} from '@/models/user'
+import {useConnStore} from '@/stores/connection'
+import {useWebsocketStore} from '@/stores/websocket'
 
 const props = defineProps<{ user: User }>()
 
@@ -20,17 +20,17 @@ const isUser = userStore.getUsername() === props.user.username
 
 <template>
   <div
-    class="user justify-center text-center mr-6 group w-36"
-    :class="{ 'cursor-pointer': !isUser, 'cursor-default': isUser }"
-    @click="!isUser ? sendOffer(user.username) : null"
+      class="user justify-center text-center mr-3 md:mr-6 group w-36"
+      :class="{ 'cursor-pointer': !isUser, 'cursor-default': isUser }"
+      @click="!isUser ? sendOffer(user.username) : null"
   >
     <div class="avatar placeholder mb-2">
       <div
-        :class="{
+          :class="{
           'group-hover:ring-4 ring-accent ring-offset-4 ring-offset-base-100': !isUser,
           'ring-4 ring-accent': conn.GetUserConnection(user.username)
         }"
-        class="bg-neutral-focus text-neutral-content rounded-full w-24"
+          class="bg-neutral-focus text-neutral-content rounded-full w-16 md:w-24"
       >
         <span class="text-3xl">{{ user.username.charAt(0) }}</span>
       </div>
