5ce1c8b34b1b66aabb3ac8c45edb02fa132fae75
0xlaurens <lauwiv@gmail.com>
2023-10-24 11:03:16 +0200

    username (FE) -> add username

--- frontend/src/components/WsConnection.vue
+++ frontend/src/components/WsConnection.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import {ref, watch} from 'vue'
 import DisconnectedIcon from '@/components/icons/DisconnectedIcon.vue'
 import ConnectedIcon from '@/components/icons/ConnectedIcon.vue'
-import { useWebsocketStore } from '@/stores/websocket'
+import {useWebsocketStore} from '@/stores/websocket'
+import {useUserStore} from "@/stores/user";
 
 const hasConnection = ref(false)
 
 const ws = useWebsocketStore()
+const user = useUserStore()
 
 watch(
-  () => ws.GetConnection(),
-  (webSocket: WebSocket | undefined) => {
-    webSocket?.addEventListener('open', () => (hasConnection.value = true))
-    webSocket?.addEventListener('close', () => (hasConnection.value = false))
-    webSocket?.addEventListener('error', () => (hasConnection.value = false))
-  }
+    () => ws.GetConnection(),
+    (webSocket: WebSocket | undefined) => {
+      webSocket?.addEventListener('open', () => (hasConnection.value = true))
+      webSocket?.addEventListener('close', () => (hasConnection.value = false))
+      webSocket?.addEventListener('error', () => (hasConnection.value = false))
+    }
 )
 </script>
 <template>
   <div class="flex justify-center align-middle space-x-3">
-    <disconnected-icon v-if="!hasConnection" />
-    <connected-icon v-if="hasConnection" />
+    <disconnected-icon v-if="!hasConnection"/>
+    <connected-icon v-if="hasConnection"/>
     <p class="text-2xl hind">
-      {{ hasConnection ? 'Connected' : 'Offline' }}
+      {{ hasConnection ? `Connected as: ${user.getUsername()}` : 'Offline' }}
     </p>
   </div>
 </template>
