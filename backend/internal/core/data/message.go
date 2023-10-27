4653196b14f1d2e5f5287828c033bcf2ecbf1f35
0xlaurens <lauwiv@gmail.com>
2023-10-27 11:14:59 +0200

    data (BE) -> core models

---
+++ backend/internal/core/data/message.go
@@ -0,0 +1,32 @@
+package data
+
+type Message struct {
+	Type   MessageType       `json:"type"`
+	From   string            `json:"from"`
+	Target string            `json:"target"`
+	User   *User             `json:"user"`
+	Users  []*User           `json:"users"`
+	Body   map[string]string `json:"body"`
+}
+
+type MessageType string
+
+var MessageTypes = struct {
+	Offer           MessageType
+	Answer          MessageType
+	NewIceCandidate MessageType
+	PeerJoined      MessageType
+	PeerLeft        MessageType
+	PeerUpdated     MessageType
+	Peers           MessageType
+	Username        MessageType
+}{
+	Offer:           "Offer",
+	Answer:          "Answer",
+	NewIceCandidate: "NewIceCandidate",
+	PeerJoined:      "PeerJoined",
+	PeerLeft:        "PeerLeft",
+	PeerUpdated:     "PeerUpdated",
+	Peers:           "Peers",
+	Username:        "Username",
+}
