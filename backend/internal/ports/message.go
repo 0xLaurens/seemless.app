4b75eac93e4a591741ed728f528683ffcee6f79e
0xlaurens <lauwiv@gmail.com>
2023-10-28 12:47:31 +0200

    port (BE) -> Message notifier

--- backend/internal/ports/message.go
+++ backend/internal/ports/message.go
@@ -18,3 +18,11 @@ type MessageHandler interface {
 	UpgradeWebsocket(c *fiber.Ctx) error
 	HandleWebsocket(c *websocket.Conn) error
 }
+
+type MessageNotifier interface {
+	Read() (*data.Message, error)
+	Send(msg *data.Message) error
+	SendTargeted(msg *data.Message, target *data.User) error
+	SendJSON(json interface{}) error
+	InvalidMessage(msg interface{}) error
+}
