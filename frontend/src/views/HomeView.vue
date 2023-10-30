01dc1118b6a39a08dec0f304d79e45ba1a3f308a
0xlaurens <lauwiv@gmail.com>
2023-10-30 13:30:11 +0100

    home (FE) -> layout

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import InfoIcon from "@/components/icons/InfoIcon.vue";
+
 let imageSrc =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Fig_7_Le_Telephone_by_T_du_Moncel_Paris_1880_%28Large%29.jpg/440px-Fig_7_Le_Telephone_by_T_du_Moncel_Paris_1880_%28Large%29.jpg'
 </script>
 
 <template>
   <section class="flex h-screen items-center justify-center py-10">
-    <!--    <div class="container h-full px-4 md:px-6 lg:flex lg:items-center lg:justify-between">-->
     <div
-        class="container flex flex-col lg:flex-row items-center max-w-6xl px-4 md:px-6 lg:items-center justify-between h-full md:h-auto md:justify-start w-full">
+        class="container flex flex-col lg:flex-row items-center max-w-6xl px-4 lg:px-6 lg:items-center justify-between h-full lg:h-auto lg:justify-start w-full">
       <div class="lg:w-1/2 space-y-4">
         <h1 class="text-4xl text-black dark:text-white font-bold tracking-tighter sm:text-5xl xl:text-6xl"> Share Files
           <span class="text-accent">Easily</span> and <span class="text-accent">Securely</span></h1>
@@ -16,7 +17,12 @@ let imageSrc =
           time in
           just a few clicks.
         </p>
-        <router-link class="btn btn-md font-normal btn-accent normal-case" to="/nick">Start Sharing</router-link>
+        <div class="flex justify-center lg:justify-start space-x-3">
+          <button class="btn btn-md font-normal btn-square btn-neutral normal-case" aria-label="">
+            <info-icon/>
+          </button>
+          <router-link class="btn btn-md font-normal btn-accent normal-case" to="/nick">Start Sharing</router-link>
+        </div>
       </div>
       <div class="lg:w-1/2">
         <img :src="imageSrc"
