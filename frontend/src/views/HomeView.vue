dfcc961da215a7eac567e9cb872e161e3cfdc908
0xlaurens <lauwiv@gmail.com>
2023-10-30 12:05:57 +0100

    layout (FE) -> home page restyle

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,36 +4,25 @@ let imageSrc =
 </script>
 
 <template>
-  <div class="max-w-6xl mx-auto px-4">
-    <section
-        class="pt-4 pb-24 md:py-24 md:mb-12 flex flex-col md:flex-row justify-center items-center md:items-start space-y-20 md:space-y-0 md:space-x-6 lg:space-x-12 text-center md:text-left">
-      <div class="w-full md:w-7/12 md:mr-8">
-        <h1 class="text-5xl lg:text-6xl font-extrabold tracking-tight mb-6 mt-6 dark:text-white">Share Files Directly
-          <span class="text-transparent bg-clip-text bg-gradient-to-b from-primary-focus to-primary whitespace-nowrap">to any device</span>
-        </h1>
-        <p class="mb-4 text-lg opacity-90">Share your files peer-to-peer from any device without ever storing it
-          online.</p>
-        <div class="inline-flex flex-col justify-center items-center my-5 gap-2">
-          <router-link class="btn btn-primary btn-wide" to="/nick">Share Now</router-link>
-        </div>
+  <section class="flex h-screen items-center justify-center py-10">
+    <!--    <div class="container h-full px-4 md:px-6 lg:flex lg:items-center lg:justify-between">-->
+    <div
+        class="container flex flex-col lg:flex-row items-center max-w-6xl px-4 md:px-6 lg:items-center justify-between h-full md:h-auto md:justify-start w-full">
+      <div class="lg:w-1/2 space-y-4">
+        <h1 class="text-4xl text-black dark:text-white font-bold tracking-tighter sm:text-5xl xl:text-6xl"> Share Files
+          <span class="text-accent">Easily</span> and <span class="text-accent">Securely</span></h1>
+        <p class="text-lg">With our platform, you can share files with anyone, anywhere, at
+          any
+          time in
+          just a few clicks.
+        </p>
+        <router-link class="btn btn-md font-normal btn-accent normal-case" to="/nick">Start Sharing</router-link>
       </div>
-      <div class="relative w-full md:w-5/12 flex flex-col justify-start items-start gap-16 md:gap-24">
-        <img :src="imageSrc" class="rounded-lg shadow-2xl" alt="tin can telephone"/>
+      <div class="lg:w-1/2">
+        <img :src="imageSrc"
+             class="mx-auto aspect-content rounded-xl object-cover sm:w-full lg:order-last"
+             alt="tin can telephone"/>
       </div>
-    </section>
-    <section>
-      <div
-          class="features grid grid-cols-12 gap-8 content-center font-bold capitalize text-2xl mb-48"
-      >
-        <div class="left col-span-12 md:col-span-7 space-y-3">
-          <h2 class="hind">🔥 Blazingly Fast</h2>
-          <h2 class="hind">♾️ No File Size Limit</h2>
-        </div>
-        <div class="right col-span-12 md:col-span-5 space-y-3 gap-16 md:gap-24">
-          <h2 class="hind">🔄 Peer-to-Peer</h2>
-          <h2 class="hind">🔒 End-to-end encryption</h2>
-        </div>
-      </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
