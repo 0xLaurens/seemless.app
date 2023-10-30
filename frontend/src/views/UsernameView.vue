25788532dfc8114bfeb4d6934f8cec9174a054c1
0xlaurens <lauwiv@gmail.com>
2023-10-30 11:25:07 +0100

    layout (FE) -> fix long name screen style break

--- frontend/src/views/UsernameView.vue
+++ frontend/src/views/UsernameView.vue
@@ -2,6 +2,7 @@
 import {useUserStore} from '@/stores/user'
 import {ref} from 'vue'
 import router from '@/router'
+import BackIcon from "@/components/icons/BackIcon.vue";
 
 const user = useUserStore()
 const username = ref(user.getUsername())
@@ -23,32 +24,74 @@ function selectNickname() {
 </script>
 
 <template>
-  <div class="flex justify-start md:justify-center">
-    <div class="max-w-6xl mx-auto align-middle justify-center px-4">
-      <section class="flex flex-col h-screen justify-between md:justify-center">
-        <div class="call-to-action pt-10 md:pt-0 md:mb-6">
-          <h1 class="max-w-2xl text-4xl font-black text-black dark:text-white capitalize mb-5">Choose a nickname</h1>
-          <p class="max-w-xl text-lg md:text-xl font-medium mb-12 hind normal-case">
-            Nicknames are used as a identifier to separate different users in a room. A room must
-            always have users with unique nicknames.
-          </p>
+  <section class="flex h-screen items-center justify-center py-10">
+    <div
+        class="flex flex-col items-center max-w-3xl justify-between h-full md:h-auto md:justify-start w-full px-3 md:px-0">
+      <div class="flex flex-row justify-between items-center w-full mb-10">
+        <div>
+          <router-link to="../" class="btn btn-md md:btn-sm btn-ghost font-black">
+            <back-icon/>
+          </router-link>
         </div>
-        <div class="pb-10 md:pb-0">
-          <form @submit="selectNickname">
-            <label class="text-xl md:text-2xl font-black text-black dark:text-white capitalize mb-5 hind">
-              Nickname<span v-if="username" class="hind font-black normal-case">: {{ username }}</span>
-            </label>
-            <input
-                type="text"
-                :value="username"
-                @input="cleanNickname(($event.target as HTMLInputElement).value)"
-                placeholder="Your cool username"
-                class="input input-primary w-full mb-5 font-regular hind"
-            />
-            <button :disabled="!username" class="btn btn-primary w-full">Select Nickname</button>
-          </form>
-        </div>
-      </section>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <h1 class="text-4xl font-black text-black dark:text-white capitalize mb-5">Choose a nickname</h1>
+        <p class="text-lg md:text-xl font-medium mb-12 hind normal-case">
+          Nicknames are used as a identifier to separate different users in a room. A room must
+          always have users with unique nicknames.
+        </p>
+      </div>
+      <div class="w-full">
+        <form @submit="selectNickname">
+          <label class="text-xl md:text-2xl font-black text-black dark:text-white capitalize mb-5 hind break-words">
+            Nickname<span v-if="username" class="hind font-black normal-case">: {{ username }}</span>
+          </label>
+          <input
+              type="text"
+              :value="username"
+              @input="cleanNickname(($event.target as HTMLInputElement).value)"
+              placeholder="Your cool username"
+              class="input input-primary w-full mb-5 font-regular hind"
+          />
+          <button :disabled="!username" class="btn btn-primary w-full">Select Nickname</button>
+        </form>
+      </div>
     </div>
-  </div>
+  </section>
+
+  <!--  <div class="flex justify-start md:justify-center">-->
+  <!--    <div class="max-w-6xl mx-auto align-middle justify-center px-4">-->
+  <!--      <section class="flex flex-col h-screen justify-between md:justify-center">-->
+  <!--        <div class="call-to-action md:mb-6">-->
+  <!--          <div>-->
+  <!--            <router-link to="../" class="btn btn-md md:btn-lg btn-ghost font-black mt-10 md:pt-0 mb-3">-->
+  <!--              <back-icon/>-->
+  <!--            </router-link>-->
+  <!--          </div>-->
+  <!--          <h1 class="max-w-2xl text-4xl font-black text-black dark:text-white capitalize mb-5">Choose a nickname</h1>-->
+  <!--          <p class="max-w-xl text-lg md:text-xl font-medium mb-12 hind normal-case">-->
+  <!--            Nicknames are used as a identifier to separate different users in a room. A room must-->
+  <!--            always have users with unique nicknames.-->
+  <!--          </p>-->
+  <!--        </div>-->
+  <!--        <div class="pb-10 md:pb-0">-->
+  <!--          <form @submit="selectNickname">-->
+  <!--            <label class="text-xl md:text-2xl font-black text-black dark:text-white capitalize mb-5 hind">-->
+  <!--              Nickname<span v-if="username" class="hind font-black normal-case">: {{ username }}</span>-->
+  <!--            </label>-->
+  <!--            <input-->
+  <!--                type="text"-->
+  <!--                :value="username"-->
+  <!--                @input="cleanNickname(($event.target as HTMLInputElement).value)"-->
+  <!--                placeholder="Your cool username"-->
+  <!--                class="input input-primary w-full mb-5 font-regular hind"-->
+  <!--            />-->
+  <!--            <button :disabled="!username" class="btn btn-primary w-full">Select Nickname</button>-->
+  <!--          </form>-->
+  <!--        </div>-->
+  <!--      </section>-->
+  <!--    </div>-->
+  <!--  </div>-->
 </template>
