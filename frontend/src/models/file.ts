b99116c5dde3d9864124da4aeda7105a9fa381c4
0xlaurens <lauwiv@gmail.com>
2023-11-04 17:32:56 +0100

    file (FE) -> offer status

--- frontend/src/models/file.ts
+++ frontend/src/models/file.ts
@@ -10,12 +10,16 @@ export interface FileOffer {
     status: FileSetup
     from: string
     files: FileMessage[]
+    current: number
 }
 
 export enum FileSetup {
-    Offer = 'Offer',
-    Accept = 'Accept',
-    Deny = 'Deny',
+    Offer = 'Offer', // status when files are being offered
+    AcceptOffer = 'AcceptOffer', // status when offer is accepted
+    DenyOffer = 'DenyOffer', // status when offer is denied
+    LatestOffer = "LatestOffer", // status for setting the latest offer
+    RequestNext = 'RequestNext', // status receiver is ready for the next file is done
+    Complete = "Complete", // status when the offer is finished
 }
 
 export enum FileStatus {
