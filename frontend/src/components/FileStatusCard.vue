364a057e06f16a19e5394f6c44e400183ee00fdd
0xlaurens <lauwiv@gmail.com>
2023-11-07 14:50:36 +0100

    round (FE) -> math.round

--- frontend/src/components/FileStatusCard.vue
+++ frontend/src/components/FileStatusCard.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
 const user = useUserStore()
 const progress: Ref<number> = ref(0);
 watch(props.file, (file) => {
-  progress.value = parseFloat((file.progress / file.size * 100).toFixed(0));
+  progress.value = Math.round((file.progress / file.size * 100));
 })
 
 import DocumentIcon from "@/components/icons/DocumentIcon.vue";
@@ -44,12 +44,6 @@ import {useUserStore} from "@/stores/user";
           <path stroke-linecap="round" stroke-linejoin="round"
                 d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
         </svg>
-        <!--        <svg v-if="progress < 100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"-->
-        <!--             stroke="currentColor"-->
-        <!--             class="w-8 h-auto ">-->
-        <!--          <path stroke-linecap="round" stroke-linejoin="round"-->
-        <!--                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>-->
-        <!--        </svg>-->
         <svg v-if="progress < 100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
              stroke="currentColor"
              class="w-6 h-auto">
