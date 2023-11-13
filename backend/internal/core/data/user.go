c31d9aa80f057faccc69612bc3a5d3bda48ea7c1
0xlaurens <lauwiv@gmail.com>
2023-11-13 15:05:16 +0100

    user -> remove type decorator

--- backend/internal/core/data/user.go
+++ backend/internal/core/data/user.go
@@ -5,22 +5,21 @@ import (
 	"net"
 )
 
-type Conn *websocket.Conn
 type RemoteAddr *net.TCPAddr
 type UserOption func(*User)
 
 type User struct {
-	Username   string     `json:"username"`
-	Device     string     `json:"device"`
-	Connection Conn       `json:"-"`
-	RemoteAddr RemoteAddr `json:"-"`
+	Username   string          `json:"username"`
+	Device     string          `json:"device"`
+	Connection *websocket.Conn `json:"-"`
+	RemoteAddr RemoteAddr      `json:"-"`
 }
 
 // WithConnection helper function for create user to pass a connection
-func WithConnection(conn Conn) UserOption {
+func WithConnection(conn *websocket.Conn) UserOption {
 	return func(u *User) {
 		u.Connection = conn
-		u.RemoteAddr = conn.Conn.RemoteAddr().(*net.TCPAddr)
+		u.RemoteAddr = conn.RemoteAddr().(*net.TCPAddr)
 	}
 }
 
@@ -49,6 +48,6 @@ func (u *User) UpdateUsername(Username string) {
 	u.Username = Username
 }
 
-func (u *User) GetConnection() Conn {
+func (u *User) GetConnection() *websocket.Conn {
 	return u.Connection
 }
