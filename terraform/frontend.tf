2c04b1ba6dc2b0d78bedcab361f9d15fa89a19ca
0xlaurens <lauwiv@gmail.com>
2023-11-16 16:17:12 +0100

    aws -> s3 static website

---
+++ terraform/frontend.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "frontend-vue" {
+  bucket = var.s3_bucket_name
+  tags = {
+    Name = "Vue Frontend"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend-config" {
+  bucket = var.s3_bucket_name
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_website_policy" {
+  bucket         = aws_s3_bucket.frontend-vue.id
+  policy = templatefile("s3-policy.json", { bucket = var.s3_bucket_name })
+}
