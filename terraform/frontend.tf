4166d957223a18deedabc1de5db45e6cbccae5a3
0xlaurens <lauwiv@gmail.com>
2023-11-17 11:46:30 +0100

    frontend -> acl update in preperation for cloudfront

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -12,23 +12,13 @@ resource "aws_s3_bucket_ownership_controls" "frontend_bucket_ownership_controls"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "aws_s3_bucket_public_access" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_acl" "frontend_aws_s3_bucket_acl" {
   depends_on = [
     aws_s3_bucket_ownership_controls.frontend_bucket_ownership_controls,
-    aws_s3_bucket_public_access_block.aws_s3_bucket_public_access,
   ]
 
   bucket = aws_s3_bucket.frontend_bucket.id
-  acl    = "public-read"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_website_configuration" "frontend_bucket_config" {
