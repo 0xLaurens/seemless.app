567e473048faf9a5e830f6d13bae57c73f01e9ea
0xlaurens <lauwiv@gmail.com>
2023-11-17 10:25:23 +0100

    frontend -> terraform acl permissions

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,21 +1,43 @@
-resource "aws_s3_bucket" "frontend-vue" {
+resource "aws_s3_bucket" "frontend_bucket" {
   bucket = var.s3_bucket_name
   tags = {
     Name = "Vue Frontend"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "frontend-config" {
-  bucket = var.s3_bucket_name
-  index_document {
-    suffix = "index.html"
-  }
-  error_document {
-    key = "index.html"
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_policy" "static_website_policy" {
-  bucket = aws_s3_bucket.frontend-vue.id
-  policy = templatefile("s3-policy.json", { bucket = var.s3_bucket_name })
+resource "aws_s3_bucket_public_access_block" "aws_s3_bucket_public_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "frontend_aws_s3_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.frontend_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.aws_s3_bucket_public_access,
+  ]
+
+  bucket = aws_s3_bucket.frontend_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_bucket_config" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
