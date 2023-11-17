9eb6009b00b8b29dba647af4567f103b2f16a136
0xlaurens <lauwiv@gmail.com>
2023-11-17 15:41:15 +0100

    terraform -> latest

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "backend-golang" {
-  ami           = "ami-042e6fdb154c830c5"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "go-backend"
-  }
-}
+#resource "aws_instance" "backend-golang" {
+#  ami           = "ami-042e6fdb154c830c5"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "go-backend"
+#  }
+#}
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = var.aws_region
 }
 
