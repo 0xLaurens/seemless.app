30d2b2b542275c8182c55fa3f828f1c0d8ea3dd7
0xlaurens <lauwiv@gmail.com>
2023-11-21 13:43:31 +0100

    vpc -> cidr and azs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "backend-vpc"
-  cidr = "10.0.0.0/24"
+  cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
