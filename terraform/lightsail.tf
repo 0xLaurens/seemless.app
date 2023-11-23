63cbed4b1b10e417e8c7074d89bc5cb69bf06840
0xlaurens <lauwiv@gmail.com>
2023-11-23 11:35:29 +0100

    lightsail -> container

---
+++ terraform/lightsail.tf
@@ -0,0 +1,62 @@
+resource "aws_lightsail_instance" "lightsail_instance" {
+  name              = "lightsail_instance"
+  bundle_id         = "micro_3_0"
+  availability_zone = "eu-central-1a"
+  blueprint_id      = "amazon_linux_2023"
+}
+
+resource "aws_lightsail_container_service" "lightsail_container_service" {
+  name  = "lightsail-container-service"
+  power = "micro"
+  scale = 1
+
+  private_registry_access {
+    ecr_image_puller_role {
+      is_active = true
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lightsail_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "default" {
+  repository = aws_ecr_repository.container_repo.name
+  policy     = data.aws_iam_policy_document.lightsail_policy.json
+}
+
+resource "aws_lightsail_container_service_deployment_version" "lightsail_container_deployment" {
+  service_name = aws_lightsail_container_service.lightsail_container_service.name
+  container {
+    container_name = "seemless-backend"
+    image          = "${aws_ecr_repository.container_repo.repository_url}:latest"
+    ports = {"3000" = "HTTP"}
+  }
+
+  public_endpoint {
+    container_name = "seemless-backend"
+    container_port = 3000
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 5
+      interval_seconds    = 30
+      path                = "/health"
+      success_codes       = "200"
+    }
+  }
+}
