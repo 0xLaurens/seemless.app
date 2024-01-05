4cadc9fe8b45634294653ac2f86b88e684ad95f2
0xlaurens <lauwiv@gmail.com>
2024-01-05 10:50:58 +0100

    accesibility -> aria-labels

--- frontend/src/components/MobileNav.vue
+++ frontend/src/components/MobileNav.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="btm-nav btm-nav-md flex md:hidden bg-base-200 z-50">
-    <router-link active-class="text-primary active bg-base-200" to="/">
+    <router-link aria-label="home" active-class="text-primary active bg-base-200" to="/">
       <home-icon/>
     </router-link>
-    <router-link active-class="text-primary active bg-base-200" to="/share">
+    <router-link aria-label="share" active-class="text-primary active bg-base-200" to="/share">
       <qr-icon/>
     </router-link>
-    <router-link active-class="text-primary active bg-base-200" to="/settings">
+    <router-link aria-label="settings" active-class="text-primary active bg-base-200" to="/settings">
       <settings-icon/>
     </router-link>
   </div>
